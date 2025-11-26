30bb561823855834f451fabf8657e0a8a57323dd
Edward Ruzek <edadiddles@hotmail.com>
2025-11-26 08:43:06 -0500

    updating grammer to match what I believe is expected by appendix I in the book

--- src/grammer.zig
+++ src/grammer.zig
@@ -4,9 +4,38 @@ const token_types = @import("token.zig");
 
 const Grammer = @This();
 
-const Expression = union(enum){
-    Literal: struct{ expr: token_types.Token, },
-    Grouping: struct{ expr: *Expression, },
-    Unary: struct{ op: token_types.Token, right: *Expression, },
-    Binary: struct{ op: token_types.Token, left: *Expression, right: *Expression, },
+pub const Program = struct{ decl: []Declaration };    
+
+pub const Declaration = union(enum){
+    ClassDecl: struct{ name: token_types.Token, subclass: ?token_types.Token, func: []Utility.Function, },
+    FuncDecl: struct{ func: Utility.Function },
+    VarDecl: struct{ name: token_types.Token, expr: Expression, }, 
+};
+
+pub const Statement = union(enum){
+    ExprStmt: struct{ expr: Expression, },
+    ForStmt: struct{ var_decl: ?Declaration.VarDecl, expr_stmt: ?Statement.ExprStmt, cond: ?Expression, incr: ?Expression, stmt: Statement, },
+    IfStmt: struct{ cond: Expression, main_stmt: Statement, else_stmt: ?Statement, },
+    PrintStmt: struct{ expr: Expression, },
+    ReturnStmt: struct{ expr: ?Expression, },
+    WhileStmt: struct{ cond: Expression, stmt: Statement },
+    Block: struct{ decl: []Declaration, },
+};
+
+pub const Expression = union(enum){
+    Assignment: struct{ call: ?Expression.Call, name: token_types.Token, asgmt: ?Expression.Assignment, logic_or: ?Expression.LogicOr, },
+    LogicOr: struct{ primary: Expression.LogicAnd, secondary: ?[]Expression.LogicAnd, },
+    LogicAnd: struct{ primary: Expression.Equality, secondary: ?[]Expression.Equality, },
+    Equality: struct{ primary: Expression.Comparison, secondary: ?[]Expression.Comparison, },
+    Comparison: struct{ primary: Expression.Term, secondary: ?[]Expression.Term, },
+    Factor: struct{ primary: Expression.Unary, secondary: ?[]Expression.Unary, },
+    Unary: struct{ unary: ?Expression.Unary, call: ?Expression.Call, },
+    Call: struct{ primary: Expression.Primary, args: ?[]Utility.Arguments, secondary: ?[]token_types.Token, },
+    Primary: struct{ literal: ?token_types.Token, expr: ?Expression, },
+};
+
+pub const Utility = union(enum){
+    Function: struct{ name: token_types.Token, params: ?Utility.Parameters, block: Statement.Block, },
+    Parameters: struct{ primary: token_types.Token, secondary: ?[]token_types.Token, },
+    Arguments: struct { primary: Expression, secondary: ?[]Expression, },
 };
